edcc3568b4329662ab0f22ac1824068ee70f85c5
Łukasz Wieczorek <wieczorek1990@gmail.com>
2014-05-13 16:45:35 +0200

    logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'history/index'
+  match '/history', to: 'history#index', via: :get
+
   get 'contact/index', to: redirect('/contact')
   match '/contact', to: 'contact#index', via: :get
 
